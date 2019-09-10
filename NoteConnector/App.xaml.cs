3fe80f53b7c9f8973bffaf2246328975fe812fb8
三黑 <w953934508@outlook.com>
2019-09-10 21:26:46 +0800

    添加项目文件。

---
+++ NoteConnector/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NoteConnector
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string[] args;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            args = e.Args;
+            if (args.Count() == 0)
+            {
+#if RELEASE
+                this.Shutdown();
+#endif
+            }
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ NoteConnector/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using utauPlugin;
+
+namespace NoteConnector
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UtauPlugin plugin;
+        Note note1;
+        Note note2;
+        public MainWindow()
+        {
+            string[] args = App.args;
+            if (args.Count() != 0)
+            {
+                plugin = new UtauPlugin(args[0]);
+            }
+            //this.Loaded += MainWindow_Loaded;
+            InitializeComponent();
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //载入插件
+            plugin.Input();
+            List<Note> notes = plugin.note;
+            //根据有无Prev来决定从第几个音符开始取
+            int i = plugin.hasPrev ? 1 : 0;
+#warning 这里需要检测最少两个音符
+            //取出前两个note用于拼字
+            note1 = notes[i];
+            note2 = notes[i+1];
+            //缩短音符得到R后Modulation(移调)默认为空会出问题，这里设置一下
+            note1.SetMod(note1.GetMod());
+            note2.SetMod(note2.GetMod());
+            plugin.Output();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow_Loaded(sender, e);
+        }
+    }
+}
